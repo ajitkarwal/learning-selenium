e1460e301634f56b0f91ef8b4bf9a5682dbf4d45
AP Singh <ajitpalsinghkarwal@gmail.com>
2021-05-18 22:32:02 +0530

    DynamicXpath and FindElement

--- src/main/java/Locators/DynamicXpath.java
+++ src/main/java/Locators/DynamicXpath.java
@@ -20,14 +20,15 @@ public class DynamicXpath {
         driver.get("https://www.ebay.com/");
         driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 
+// BASIC XPATHS:
         //using 'id' attribute in xpath
         //    WebElement searchBox = driver.findElement(By.xpath("//input[@id='gh-ac']"));
-
 
         //using 'class' attribute in xpath
      //   WebElement searchBox = driver.findElement(By.xpath("//input[@class='gh-tb ui-autocomplete-input']"));
 
-        //using 'contains()' method in xpath. to check if an attribute contains some text.
+//CONTAINS() METHOD:
+    // using 'contains()' method in xpath. to check if an attribute contains some text.
         WebElement searchBox = driver.findElement(By.xpath("//input[contains(@class,'gh-tb ui-autocomplete-input')]"));
 
         searchBox.sendKeys("Java");
@@ -35,6 +36,22 @@ public class DynamicXpath {
 
 
         System.out.println("Entered Java in the Search Box");
+//DYNAMIC IDs:
+        //sometimes id is dynamic, and changes everytime like :
+        // id = 'test_1234  OR         // id = 'test_abcd  OR         // id = 'test_1a
+
+        //this is when contains() is very helpful,
+
+        driver.findElement(By.xpath("//input[@id='test_']"));
+        // this means that find a particular input tag in which the id starts with test_
+
+//Starts-With() METHOD:
+        driver.findElement(By.xpath("//input[starts-with(@id,'test')]"));
+        //it will try to find 'id' attribute in 'input' tag whose value starts with 'test'.
+
+//FOR LINKS (all the links are represented by <a> tag:
+        driver.findElement(By.xpath("//a[contains(text(),'Sign in')]"));
+
 
 
         driver.quit();
---
+++ src/main/java/Locators/FindElement.java
@@ -0,0 +1,65 @@
+package Locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class FindElement {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.get("https://www.ebay.com/");
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        //get the count of links on the page and
+        System.out.println("No of links in the page = " + links.size());
+
+        //text of each link
+
+        //WHILE LOOP
+//        Iterator<WebElement> itr = links.iterator();//An iterator over a collection.
+//        while (itr.hasNext()) {
+//            System.out.println(itr.next().getText());
+//
+//        }
+
+        //While Loop without iterator.
+        int j = 0;
+        while (j < links.size()) {
+            String text = links.get(j).getText();
+            if(text.length()!=0) { // to print only non-null strings
+                System.out.print(text + "  ");
+                System.out.println(text.length());
+            }
+
+            j++;
+        }
+
+        //OR using for loop with iterator
+//        for (Iterator<WebElement> it = links.iterator(); it.hasNext(); ) {
+//            System.out.println(it.next().getText());
+//        }
+
+        //For Loop without iterator
+//        for(int i=0;i<links.size();i++){
+//            String text = links.get(i).getText();
+//            System.out.println(text);
+//        }
+
+        driver.quit();
+    }
+}
